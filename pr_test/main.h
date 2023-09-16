dc350c6d6cf7fa9a7d4d046005b8d67bf4c4df21
Tahani-Saber <tahani.saber.habib11@gmail.com>
2023-09-16 08:45:18 -0700

    add main.h file included the libs and the prototypes for print the char and str

---
+++ pr_test/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdarg.h>
+
+int _putchar(char c);
+int _strlen(char *s);
+int _print_str(va_list args);
+int _print_char(va_list args);
+
+
+#endif
