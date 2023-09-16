fcb8ae1065d68c87ff24acd2d77f1b8eb6780466
Tahani-Saber <tahani.saber.habib11@gmail.com>
2023-09-16 11:01:41 -0700

    upload the file.c for the print_100symbol file

---
+++ pr_test/print_100symbol.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _print_symbol - function to print symbol
+ * @format: the fixed argument of the function
+ *
+ * Return: nothing
+ */
+
+void _print_symbol(char *format, ...)
+{
+	va_list args;
+	va_start(args, format);
+
+	case '%':
+		s_count += _print_char('%');
+	break;
+	va_end(args);
+}
