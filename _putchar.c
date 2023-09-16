272212ad9367128043bc5f6033ead2c653d6bcbe
Abdelrahman Mohamed <3bdelrahmanm0o@gmail.com>
2023-09-17 00:06:20 +0300

    Moving _putchar.c and main.h to production

---
+++ _putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _putchar - function that print / write char
+ *
+ * @c: the arg. present the char to print
+ *
+ * Return: 1 on success
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
---
+++ main.h
@@ -0,0 +1,29 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdarg.h>
+
+int _putchar(char);
+int _strlen(char *);
+int print_str(va_list);
+int print_char(va_list);
+int print_100symbol(va_list);
+int _printf(const char *format, ...);
+int (*get_op_func(char))(va_list);
+
+/**
+ * struct op - Struct op
+ *
+ * @op: the specifier
+ * @func: the function associated
+ */
+typedef struct op
+{
+	char *op;
+	int (*func)(va_list);
+} op_t;
+
+#endif /* MAIN_H */
