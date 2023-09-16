effce9bdc34f765cecc977e7992644b944fd1bf8
Abdelrahman Mohamed <3bdelrahmanm0o@gmail.com>
2023-09-16 21:29:03 +0300

    test files

---
+++ pr_test/main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - print formated string
+ *
+ * Return: always success (0), otherwise (1).
+ */
+int main(void)
+{
+	printf("hello-%s, %i <<\n", "test", 55, 88);
+	return (0);
+}
