5f7856f430389aa76cb47bdf5d1a74df11869282
Abdelrahman Mohamed <3bdelrahmanm0o@gmail.com>
2023-09-19 16:28:04 +0300

    Add get_flags.c file to production

---
+++ pr_test/get_flags.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * get_flags - set flags
+ *
+ * @format: formated string.
+ * @index: index of the flag in the formated string.
+ * @flags: flags' struct
+ *
+ * Return: 1 => flag found, otherwise 0.
+ */
+int get_flags(const char *format, int index, flags_t *flags)
+{
+        int flag_found = 0;
+
+        if (format[index] == '#')
+        {
+                flags->hash = 1;
+                flag_found = 1;
+        }
+        else if (format[index] == ' ')
+        {
+                flags->space = 1;
+                flag_found = 1;
+        }
+        else if (format[index] == '+')
+        {
+                flags->plus = 1;
+                flag_found = 1;
+        }
+
+        return (flag_found);
+}
