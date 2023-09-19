7fac6a4e372665e8f35727072bd66fb718ccdff2
Abdelrahman Mohamed <3bdelrahmanm0o@gmail.com>
2023-09-19 20:43:37 +0300

    print_STR.c returns wrong len

--- print_STR.c
+++ print_STR.c
@@ -21,19 +21,22 @@ int print_STR(va_list args, flags_t *flag)
 	s_count = _strlen(str);
 	for (index = 0; str[index]; index++)
 	{
-		if (str[index] >= 32 && str[index] < 127)
+		if ((str[index] >= 0 && str[index] < 32) || str[index] >= 127)
 		{
-			_putchar(str[index]);
-			continue;
+			_putchar('\\');
+			_putchar('x');
+			s_count += 2;
+			/* All numbers below 16 are one digit hexadecimal */
+			/* So we want to print always two digit hexadecimal */
+			if (str[index] < 16)
+			{
+				_putchar('0');
+				s_count++;
+			}
+			s_count += convert((unsigned int)str[index], 16, 1);
 		}
-		/* All numbers below 16 are one digit hexadecimal */
-		/* So we want to print always two digit hexadecimal */
-		_putchar('\\');
-		_putchar('x');
-		if (str[index] < 16)
-			_putchar('0');
-
-		s_count += 2 + convert((unsigned int)str[index], 16, 1);
+		else
+			_putchar(str[index]);
 	}
 	return (s_count);
 }
